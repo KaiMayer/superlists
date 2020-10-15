593279a93aed745e1484e46665e86ce960f5392f
Kai Mayer <prinzmayer13@gmail.com>
2020-10-15 20:53:37 +0200

    new URL + view for adding to existing lists. Few tests ain't work :(

--- lists/urls.py
+++ lists/urls.py
@@ -1,4 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('lists/new', views.new_list, name='new_list'),
+    re_path(r'^lists/(\d+)/', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+]
