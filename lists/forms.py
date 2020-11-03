29254cdae4fe843ab08841c503b7e7212acfe5e1
Kai Mayer <neurokai@protonmail.com>
2020-11-03 16:35:29 +0100

    Add more complex  form uniqueness validation

--- lists/forms.py
+++ lists/forms.py
@@ -1,18 +1,12 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 
 class ItemForm(forms.models.ModelForm):
-    # item_text = forms.CharField(
-    #     widget=forms.fields.TextInput(attrs={
-    #         'placeholder': 'Enter a to-do item',
-    #         'class': 'form-control input-lg',
-    #     }),
-    # )
-
     class Meta:
         model = Item
         fields = ('text',)
@@ -29,3 +23,16 @@ class ItemForm(forms.models.ModelForm):
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
+
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super(ExistingListItemForm, self).__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
