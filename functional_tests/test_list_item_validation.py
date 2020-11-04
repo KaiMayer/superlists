b34a99af40a40ccb249fe0d965327bc79b3760bd
Kai Mayer <neurokai@protonmail.com>
2020-11-04 02:40:47 +0100

    added ft test for hiding error messages

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,6 +5,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
 
         # Edith goes to the home page and accidentally tries to submit
@@ -52,9 +55,28 @@ class ItemValidationTest(FunctionalTest):
         # She accidentally tries to enter a duplicate item
         self.get_item_input_box().send_keys('Buy wellies')
         self.get_item_input_box().send_keys(Keys.ENTER)
-        error_message = self.browser.find_elements_by_css_selector('.has-error')
+        error_message = self.get_error_element()
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(error_message[0].text,
             "You've already got this in your list"
         ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a list and causes a validation error:
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see that the error message disappears
+        self.wait_for(lambda: self.assertFalse(self.get_error_element().is_displayed()))
